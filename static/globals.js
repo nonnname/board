709f4b6c65185eb30bd24876515764b25b8e127b
nonnname <zhdanovsn@gmail.com>
2014-01-09 22:54:03 +0400

    remove unused

--- static/globals.js
+++ static/globals.js
@@ -2,14 +2,6 @@ function nl2br (str) {
     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br />$2');
 }
 
-function idToProject(id) {
-  return {"883372" : "B", "597637": "R"}[id];
-}
-
-function idToIcon(id) {
-  return {"883372" : "https://lh6.ggpht.com/89g3m4FaSNjBKjfQOkhYTj58EtBOP-DQz1Ck6fILuEkCE_o9iV1VSIgqgbLKsn3_Vg=w300", "597637": "https://lh6.ggpht.com/fJUZE0OYXy0XDQbdAE-mefV5NmZVO-7jx5INEGTuYZOd61i8vMG-pexe80Go95v5lw=w300"}[id];
-}
-
 //todo: disable any try to save models
 //Backbone.sync = function() {}
 
@@ -28,7 +20,6 @@ var D = new Dashboard({ storage: $.localStorage });
   new VModalDetails({ model: D, el : document.getElementById("modal-details") });
 
   if(!S.isEmpty("token")) {
-    console.log("start with token");
     D.start(S.get("token"));
   }
   // DEBUG $(".modal").modal({ show: true});
